5bd6b1f70b820fad6bc7f9f5dfc730cb71fe0dcf
nairo <nairo.elsner@hotmail.com>
2023-11-06 22:51:30 -0300

    Change endpoint_url comment

--- Solutions/Nairo/exercise-1/app/env.py
+++ Solutions/Nairo/exercise-1/app/env.py
@@ -1,7 +1,7 @@
 import os
 
-ENDPOINT_URL = 'http://192.168.42.33:4566'
-#ENDPOINT_URL = os.getenv('ENDPOINT_URL', 'http://host.docker.internal:4566')
+#ENDPOINT_URL = 'http://192.168.42.33:4566'
+ENDPOINT_URL = os.getenv('ENDPOINT_URL', 'http://host.docker.internal:4566')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'test')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', 'test')
 REGION_NAME = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
